a3234bea22552353398f89db5ff5cc5c6c8f14a5
Harmanpreet Singh <harmanpreet93@users.noreply.github.com>
2016-04-29 12:17:36 +0530

    Delete scripts_index.js

--- js/scripts_index.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-
-    /*
-        Fullscreen background
-    */
-    $.backstretch([
-                   "img/backgrounds/1.jpg"
-                 ], {duration: 3000, fade: 750});
-});
